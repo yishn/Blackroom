fe769f3ceccb5a274eefbfa4eac9519e6921d1c8
Yichuan Shen <shen.yichuan@gmail.com>
2015-08-24 22:43:48 +0200

    Set according image size and animate

--- blackroom.js
+++ blackroom.js
@@ -4,12 +4,43 @@ var process = remote.require('process')
 var dialog = remote.require('dialog')
 
 function loadImage(path) {
-    console.log(path)
+    // First get image dimensions
+
+    var img = $('#test').attr('src', path)
+    var screenSize = [$('#overlay').width(), $('#overlay').height()]
+    var maxSize = [screenSize[0] * 0.8, screenSize[1] * 0.8]
+
+    img.on('load', function() {
+        var size = [img.width(), img.height()]
+        var resizedSize = size
+
+        if (size[0] > maxSize[0]) {
+            var height = maxSize[0] / size[0] * size[1]
+            resizedSize = [Math.round(maxSize[0]), Math.round(height)]
+        }
+        if (size[1] > maxSize[1]) {
+            var width = maxSize[1] / size[1] * size[0]
+            resizedSize = [Math.round(width), Math.round(maxSize[1])]
+        }
+
+        $('#box').addClass('show')
+            .children('.inner')
+            .css('width', resizedSize[0])
+            .css('height', resizedSize[1])
+    })
+}
+
+function closeBox() {
+    $('#overlay').removeClass('show')
+    $('#box').removeClass('show')
+        .children('.inner')
+        .css('width', 50)
+        .css('height', 50)
 }
 
 $(window).on('load', function() {
     $('#overlay').addClass('show').on('click', function() {
-        $('#overlay').removeClass('show')
+        closeBox()
         setTimeout(remote.getCurrentWindow().close, 1000)
     })
 
