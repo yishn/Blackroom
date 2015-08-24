ddbdd50ad232c3fdce234eed7bd52a1e35535190
Yichuan Shen <shen.yichuan@gmail.com>
2015-08-24 21:56:54 +0200

    Add error message when there are no arguments

--- blackroom.js
+++ blackroom.js
@@ -1,5 +1,11 @@
 var remote = require('remote')
 var app = remote.require('app')
+var process = remote.require('process')
+var dialog = remote.require('dialog')
+
+function loadImage(path) {
+    console.log(path)
+}
 
 $(window).on('load', function() {
     $('body').addClass('show')
@@ -8,4 +14,11 @@ $(window).on('load', function() {
         $('body').removeClass('show')
         setTimeout(remote.getCurrentWindow().close, 1000)
     })
+
+    loadImage(process.argv[1])
 })
+
+if (process.argv.length < 2) {
+    dialog.showErrorBox(app.getName(), 'Please run ' + app.getName() + ' by opening an image file.')
+    app.quit()
+}
